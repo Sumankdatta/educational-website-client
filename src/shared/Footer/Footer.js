5e39e9c1291904dedd39efde2ca5baa11109c431
Sumankdatta <sumankdatta18@gmail.com>
2022-10-27 23:47:29 +0600

    footer added

--- src/shared/Footer/Footer.js
+++ src/shared/Footer/Footer.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
 
 const Footer = () => {
     return (
-        <div>
-            <h1>this is footer</h1>
-        </div>
+        <Card className="text-center">
+       
+        <Card.Body>
+          
+          <Card.Text>
+          Copyright © 2022 Easy Learn
+          </Card.Text>
+          
+        </Card.Body>
+       
+      </Card>
     );
 };
 
