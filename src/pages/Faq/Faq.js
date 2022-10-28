2763e5e324167050086f71876b84e52dc17a7466
Sumankdatta <sumankdatta18@gmail.com>
2022-10-28 14:28:04 +0600

    faq added

--- src/pages/Faq/Faq.js
+++ src/pages/Faq/Faq.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import Accordion from 'react-bootstrap/Accordion';
 
 const Faq = () => {
     return (
-        <div>
-            <h1>This is faq</h1>
-        </div>
+        <Accordion className='w-50 mx-auto mt-5'>
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>1 .what is cors?</Accordion.Header>
+        <Accordion.Body>
+        Cross-Origin Resource Sharing (CORS) is an HTTP-header based mechanism that allows a server to indicate any origins (domain, scheme, or port) other than its own from which a browser should permit loading resources. CORS also relies on a mechanism by which browsers make a "preflight" request to the server hosting the cross-origin resource, in order to check that the server will permit the actual request. In that preflight, the browser sends headers that indicate the HTTP method and headers that will be used in the actual request. An example of a cross-origin request: the front-end JavaScript code served from https://domain-a.com uses
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>2. Why are you using firebase? </Accordion.Header>
+        <Accordion.Body>
+        Firebase Authentication provides backend services, easy-to-use SDKs, and ready-made UI libraries to authenticate users to your app. It supports authentication using passwords, phone numbers, popular federated identity providers like Google, Facebook and Twitter, and more.Most apps need to know the identity of a user. Knowing a user's identity allows an app to securely save user data in the cloud and provide the same personalized experience across all of the user's devices. Firebase Authentication provides backend services, easy-to-use SDKs, and ready-made UI libraries to authenticate users to your app.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="2">
+        <Accordion.Header>3 .How does the private route work?</Accordion.Header>
+        <Accordion.Body>
+        The private route component is similar to the public route, the only change is that redirect URL and authenticate condition. If the user is not authenticated he will be redirected to the login page and the user can only access the authenticated routes If he is authenticated (Logged in).Private Routes in React Router (also called Protected Routes) require a user being authorized to visit a route (read: page). So if a user is not authorized for a specific page, they cannot access it. The most common example is authentication in a React application where a user can only access the protected pages when they are authorized (which means in this case
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="3">
+        <Accordion.Header>4 .What is Node? How does Node work?</Accordion.Header>
+        <Accordion.Body>
+        Node allows developers to write JavaScript code that runs directly in a computer process itself instead of in a browser. Node can, therefore, be used to write server-side applications with access to the operating system, file system, and everything else required to build fully-functional applications.Node.js is an open-source backend javascript runtime environment. It is a used as backend service where javascript works on the server-side of the application.
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
     );
 };
 
